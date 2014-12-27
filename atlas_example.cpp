299fe330010f250901554b7f0d933bdd516228a2
Christian Glöckner <cgloeckner@freenet.de>
2014-12-27 10:53:36 +0100

    Atlas Mapper

--- atlas_example.cpp
+++ atlas_example.cpp
@@ -1,76 +1,22 @@
-#include <unordered_map>
-#include <image_atlas.hpp>
-
-namespace sf {
-
-using UintRect = sf::Rect<unsigned int>;
-
-}
-
-class MyPacker {
-	private:	
-		using ImgPair = std::pair<std::string, sf::Image>;
-
-		std::vector<ImgPair> images;
-		std::unordered_map<std::string, sf::UintRect> clipping;
-
-	public:
-		void addImage(std::string const & filename);
-
-		sf::Image generate(std::size_t size);
-		sf::UintRect getClipping(std::string const & filename) const;
-};
-
-// ----------------------------------------------------------------------------
-
-#include <algorithm>
-#include <cassert>
-
-void MyPacker::addImage(std::string const & filename) {
-	images.push_back(std::move(make_pair(filename, sf::Image{})));
-	assert(images.back().second.loadFromFile(filename));
-}
-
-sf::Image MyPacker::generate(std::size_t size) {
-	ImageAtlas atlas{size};
-	clipping.clear();
-	sf::Vector2u pos;
-	// sort images by size (descending)
-	std::sort(images.begin(), images.end(),
-		[](ImgPair const & left, ImgPair const & right) {
-		auto size_a = left.second.getSize();
-		auto size_b = right.second.getSize();
-		return size_a.x * size_a.y > size_b.x * size_b.y;
-	});
-	// add images to atlas
-	for (auto const & pair: images) {
-		if (atlas.add(pair.second, pos)) {
-			clipping[pair.first] = sf::UintRect{pos, pair.second.getSize()};
-		}
-	}
-	return std::move(atlas.generate());
-}
-
-sf::UintRect MyPacker::getClipping(std::string const & filename) const {
-	return clipping.at(filename);
-}
-
-// ----------------------------------------------------------------------------
-
 #include <iostream>
+#include <cassert>
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <Thor/Animation.hpp>
 
+#include "image_atlas.hpp"
+
 int main() {
 	// ------------------------------------------------------------------------
 	// Create Image Atlas
-
-	MyPacker packer;
+	AtlasMapper<std::string> mapper;
 	for (auto i = 1u; i <= 5; ++i) {
-		packer.addImage("wraith-s-attack-"+std::to_string(i)+".png");
+		sf::Image tmp;
+		std::string fname{"wraith-s-attack-"+std::to_string(i)+".png"};
+		assert(tmp.loadFromFile(fname));
+		mapper.addImage(fname, std::move(tmp));
 	}
-	auto atlas = packer.generate(256u);
+	auto atlas = mapper.generate(256u);
 	atlas.saveToFile("out.png");
 
 	// ------------------------------------------------------------------------
@@ -78,7 +24,7 @@ int main() {
 	thor::FrameAnimation attack;
 	for (auto i = 1u; i <= 5; ++i) {
 		std::string fname{"wraith-s-attack-"+std::to_string(i)+".png"};
-		attack.addFrame(1.f, static_cast<sf::IntRect>(packer.getClipping(fname)));
+		attack.addFrame(1.f, mapper.getClipping(fname));
 	}
 	
 	thor::Animator<sf::Sprite, std::string> animator;
--- image_atlas.hpp
+++ image_atlas.hpp
@@ -2,7 +2,10 @@
 #include <SFML/Graphics/Image.hpp>
 #include <SFML/Graphics/Rect.hpp>
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 
+/// Helper class
 class ImageAtlas {
 	private:
 		struct Chunk {
@@ -23,5 +26,49 @@ class ImageAtlas {
 		bool add(sf::Image const & image, sf::Vector2u& offset);
 		/// Generate the final image
 		sf::Image generate() const;
+};
+
+// ----------------------------------------------------------------------------
+
+/// Maps images (identified by key) to a clipping rectangle within the altas
+template <typename Key>
+class AtlasMapper {
+	private:
+		using ImgPair = std::pair<Key, sf::Image>;
+		std::vector<ImgPair> images;
+		std::unordered_map<Key, sf::IntRect> clipping;
+
+	public:
+		void addImage(Key const & key, sf::Image&& image) {
+			auto pair = std::make_pair(key, std::move(image));
+			images.push_back(pair);
+		}
+		
+		sf::Image generate(std::size_t size) {
+			ImageAtlas atlas{size};
+			clipping.clear();
+			sf::Vector2u pos;
+			// sort images by size (descending)
+			std::sort(images.begin(), images.end(),
+				[](ImgPair const & left, ImgPair const & right) {
+				auto size_a = left.second.getSize();
+				auto size_b = right.second.getSize();
+				return size_a.x * size_a.y > size_b.x * size_b.y;
+			});
+			// add images to atlas
+			for (auto const & pair: images) {
+				if (atlas.add(pair.second, pos)) {
+					clipping[pair.first] = sf::IntRect{
+						static_cast<sf::Vector2i>(pos),
+						static_cast<sf::Vector2i>(pair.second.getSize())
+					};
+				}
+			}
+			return std::move(atlas.generate());
+		}
+
+		sf::IntRect getClipping(Key const & key) const {
+			return clipping.at(key);
+		}
 };
 
